2de336fb8812a4a30f5fa19513caa69df7d737fd
Brandom <brandom1225@foxmail.com>
2022-05-09 17:46:45 +0800

    update: 补全站点信息

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <a class="fork-me" href="https://github.com/brandom1225/100-classical-fe"></a>
     <router-view />
   </div>
 </template>
@@ -9,3 +10,16 @@ export default {
   name: 'App'
 }
 </script>
+
+<style scoped>
+.fork-me {
+  display: block;
+  width: 138px;
+  height: 138px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  transform: rotate(90deg);
+  background: transparent url(./asset/img/fork-sprite.png) 0 0 no-repeat;
+}
+</style>
